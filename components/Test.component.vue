6268a56da8e8afc9fe18acf454d250c97a02e569
teionhozonn <teionhozonn@gmail.com>
2022-03-31 20:25:04 +0900

    api test

--- components/Test.component.vue
+++ components/Test.component.vue
@@ -1,14 +1,5 @@
-<template>
-    <Html lang="ja">
-        <Head>
-            <Title>{{ title }}</Title>
-            <!-- <Meta name="description" :content="'${ title } page'"></Meta> -->
-        </Head>
-    </Html>
-    <h1>{{ title }}</h1>
-</template>
-
 <script setup lang="ts">
+
 useMeta({
     meta: [{
         name: "description"
@@ -17,7 +8,30 @@ useMeta({
 })
 
 const title = "Hello Nuxt3!!"
+
+
+const { data: users }: any = await useFetch("/api/users");
 </script>
+
+
+
+
+
+<template>
+    <Html lang="ja">
+        <Head>
+            <Title>{{ title }}</Title>
+            <!-- <Meta name="description" :content="'${ title } page'"></Meta> -->
+        </Head>
+    </Html>
+    <h1>{{ title }}</h1>
+
+    <h1>User!</h1>
+    <ul>
+        <li v-for="user in users" :key="user.id">{{ user.username }}. ......{{ user.email }}</li>
+    </ul>
+</template>
+
 
 <style scoped>
 </style>
