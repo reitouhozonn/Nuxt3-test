dcd4b28a854d505de2539402b667d70a44cbf270
teionhozonn <teionhozonn@gmail.com>
2022-03-31 20:31:58 +0900

    middleware test

--- server/middleware/logging.ts
+++ server/middleware/logging.ts
@@ -1,6 +1,6 @@
 import { IncomingMessage, ServerResponse } from "http"
 
 export default async (req: IncomingMessage, res: ServerResponse) => {
-    console.log("test")
+    console.log("middleware test")
     // console.log(req.headers)
 }
